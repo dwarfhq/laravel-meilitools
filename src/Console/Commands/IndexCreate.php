658af791a7ca4439e4305f1449eb854beec9bbb0
Andrew Minion <andrew@andrewrminion.com>
2022-11-11 14:50:24 -0600

    make index name optional

--- src/Console/Commands/IndexCreate.php
+++ src/Console/Commands/IndexCreate.php
@@ -17,7 +17,7 @@ class IndexCreate extends Command
      *
      * @var string
      */
-    protected $signature = 'meili:index:create {index : Index name}';
+    protected $signature = 'meili:index:create {index? : Index name}';
 
     /**
      * The console command description.
